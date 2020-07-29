0d825880c4a1156e6e79802b6ad09d2dbdf218f8
Leonardo Rocha <leonardo.rocha@stgen.com>
2020-07-28 23:48:51 -0300

    PageDefault and New Video

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import Button from '../Button'
+//import ButtonLink from './components/ButtonLink'
 
-function Menu(){
+function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Aluraflix"></img>
-            </a>
+            </Link>
 
-            <a className="ButtonLink" href="/">Novo Vídeo</a>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo Vídeo
+            </Button>
         </nav>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import CadastroVideo from './pages/cadastro/Video';
+
+const Pagina404 = () => (<div>404 - Not found</div>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
