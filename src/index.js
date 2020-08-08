6d5d6048783382d04939f06ee113aaa6b4cc0bf4
Leonardo Rocha <leonardo.rocha@stgen.com>
2020-08-08 16:20:49 -0300

    Updates  last class

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-const Pagina404 = () => (<div>404 - Not found</div>)
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} exact />
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
